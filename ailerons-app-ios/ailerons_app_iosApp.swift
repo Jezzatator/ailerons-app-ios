6c078078b76f225af5d5a67fadeced2933963f05
Jezzatator <jeremie.patot@protonmail.ch>
2023-12-15 14:32:37 +0100

    commit initial

---
+++ ailerons-app-ios/ailerons_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ailerons_app_iosApp.swift
+//  ailerons-app-ios
+//
+//  Created by Jérémie - Ada on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ailerons_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
