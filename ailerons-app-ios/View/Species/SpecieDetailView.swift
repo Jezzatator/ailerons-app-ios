3f8a5a4dfd3ac874ee538f0d022799f64015cf01
Jezzatator <jeremie.patot@protonmail.ch>
2024-01-18 10:44:38 +0100

    refonte page ScpiecieDetailView

--- ailerons-app-ios/View/Species/SpecieDetailView.swift
+++ ailerons-app-ios/View/Species/SpecieDetailView.swift
@@ -6,49 +6,66 @@
 //
 
 import SwiftUI
+import Charts
 
 struct SpecieDetailView: View {
-    let individual: SupaIndiv
-    @State var isPresented: Bool = true
     
-    var body: some View {
-        ZStack {
-            MapViewControllerWrapper()
-                .ignoresSafeArea()
-                .sheet(isPresented: $isPresented, content: {
-                    
-                    VStack {
-                            Text(individual.individualName)
-                                .font(.title)
-                            HStack {
-                                Text("\(individual.binomialName) / \(individual.commonName)")
-                                    .font(.title2)
-                                    .italic()
-                            }
-                            
-                            HStack {
-                                Text("Age: \(String(individual.age))")
-                                Text("Poid: \(String(individual.weight))")
-                                Text("Sexe: \(individual.sex)")
-                            }
-                            .font(.title3)
-                            
-                            Text(individual.description)
-                                .multilineTextAlignment(.leading)
-                                .padding()
-                        Spacer()
+    let individual: SupaIndiv
+    
+    var data: [ToyShape] = [
+        .init(type: "Cube", count: 5),
+        .init(type: "Sphere", count: 4),
+        .init(type: "Pyramid", count: 4)
+    ]
 
-                    }
-                    .padding(.top)
-                    .presentationDetents([.height(150), .medium, .large])
-                    .presentationBackground(.regularMaterial)
-                    .presentationBackgroundInteraction(.enabled)
-                    .interactiveDismissDisabled()
-                    .presentationContentInteraction(.scrolls)
-                    .presentationDragIndicator(.hidden)
+    var body: some View {
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("\(individual.binomialName) / \(individual.commonName)")
+                        .font(.title2)
+                        .italic()
                 }
-                )
+                
+                HStack {
+                    Text("Age: \(String(individual.age))")
+                    Text("Poid: \(String(individual.weight))")
+                    Text("Sexe: \(individual.sex)")
+                }
+                .font(.title3)
+                .padding(.bottom)
+                
+                MapViewControllerWrapper()
+                    .allowsHitTesting(false)
+                    .frame(height: 200)
+                    .padding(.bottom)
+                
+                Chart {
+                    LineMark(
+                        x: .value("Shape Type", data[0].type),
+                        y: .value("Total Count", data[0].count)
+                    )
+                    LineMark(
+                         x: .value("Shape Type", data[1].type),
+                         y: .value("Total Count", data[1].count)
+                    )
+                    LineMark(
+                         x: .value("Shape Type", data[2].type),
+                         y: .value("Total Count", data[2].count)
+                    )
+                }
+                    .frame(height: 200)
+                    .padding(.bottom)
+                
+                Text(individual.description)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Spacer()
+                
+            }
+            .padding(.top)
         }
+        .navigationTitle(individual.individualName)
     }
 }
 
@@ -57,4 +74,10 @@ struct SpecieDetailView: View {
 Et deserunt sint amet id do in adipiscing sint laborum ut eiusmod ullamco officia fugiat veniam irure consectetur. Ut occaecat nisi pariatur occaecat ut elit incididunt deserunt ad mollit. Eiusmod officia laboris quis laborum non eu anim dolore pariatur reprehenderit eiusmod sit tempor exercitation eiusmod. Incididunt aliqua labore non consectetur aliqua lorem officia id eu ex reprehenderit sunt voluptate in exercitation occaecat consectetur sint. Et quis irure cupidatat adipiscing anim voluptate laboris enim ullamco aute. Velit ipsum nulla fugiat culpa exercitation ex adipiscing esse tempor culpa culpa irure tempor laborum.
 Commodo laborum lorem minim nulla lorem elit duis quis nostrud quis eu in lorem cillum aliquip. Occaecat et sunt exercitation qui fugiat enim duis dolor pariatur irure sunt. Minim enim laborum commodo officia occaecat dolor ipsum ad incididunt et. Non lorem ipsum dolore in lorem mollit excepteur labore voluptate reprehenderit laborum sunt incididunt sed amet excepteur mollit. Nostrud incididunt cupidatat consequat aliquip anim labore anim est occaecat in ad minim pariatur proident et voluptate anim commodo. Exercitation exercitation lorem et lorem ullamco esse do sunt reprehenderit sint anim laborum cillum ex culpa ullamco sit cillum.
 """, icon: 3, picture: "test.jpeg"))
+}
+        
+struct ToyShape: Identifiable {
+            var type: String
+            var count: Double
+            var id = UUID()
 }
